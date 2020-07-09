cb197bd8ea5de9a420b53f9c6f3e6b1c7edf083b
Elvic <43492276@qq.com>
2020-07-09 11:22:21 +0800

    Add initial files for implementation.

---
+++ AsyncTest.cpp
@@ -0,0 +1,15 @@
+﻿////////////////////////////////////////////////
+//                                            //
+//           Asynchronous API Test            //
+//                                            //
+////////////////////////////////////////////////
+
+#include "pch.h"
+#include <iostream>
+
+#include "NxAsync.h"
+
+int main(int argc, char * const argv[])
+{
+	return 0;
+}
---
+++ NxAsync.h
@@ -0,0 +1,14 @@
+///////////////////////////////////////////////////////////////////
+//                                                               //
+//             Asynchronous APIs for Task Management             //
+//                                                               //
+// Author : Elvic Liang                                          //
+// Date   : 2020/6/29                                            //
+//                                                               //
+///////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <atomic>
+#include <cassert>
+
