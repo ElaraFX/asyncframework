cd6e95e03b00ad8abb71300083adf05889c95fc8
Elvic <43492276@qq.com>
2020-07-09 15:02:05 +0800

    Initial implementation of asynchronous APIs.

--- AsyncTest.cpp
+++ AsyncTest.cpp
@@ -7,7 +7,160 @@
 #include <iostream>
 #include "NxAsync.h"
 
+class CPUDevice
+{
+public:
+	CPUDevice()
+	{
+	}
+
+	~CPUDevice()
+	{
+	}
+
+	void uploadResource1()
+	{
+		std::cout << __FUNCTION__ << std::endl;
+	}
+
+	void uploadResource2()
+	{
+		std::cout << __FUNCTION__ << std::endl;
+	}
+};
+
+class GPUDevice
+{
+protected:
+	int passIndex;
+
+public:
+	GPUDevice()
+		: passIndex(0)
+	{
+	}
+
+	~GPUDevice()
+	{
+	}
+
+	void setup()
+	{
+		std::cout << __FUNCTION__ << std::endl;
+	}
+
+	bool getNewJobs()
+	{
+		std::cout << __FUNCTION__ << std::endl;
+
+		++passIndex;
+		if (passIndex >= 4)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+
+	void computeStep1()
+	{
+		std::cout << __FUNCTION__ << std::endl;
+	}
+
+	void computeStep2()
+	{
+		std::cout << __FUNCTION__ << std::endl;
+	}
+
+	void computeStep3()
+	{
+		std::cout << __FUNCTION__ << std::endl;
+	}
+};
+
+void runSerial()
+{
+	CPUDevice CPU;
+	GPUDevice GPU;
+
+	GPU.setup();
+
+	do
+	{
+		GPU.computeStep1();
+
+		CPU.uploadResource1();
+
+		GPU.computeStep2();
+
+		GPU.computeStep3();
+
+		CPU.uploadResource2();
+	}
+	while (GPU.getNewJobs());
+}
+
+void runParallel()
+{
+	Pipeline pipeline;
+	CPUDevice CPU;
+	GPUDevice GPU;
+
+	TimeSlot & slot0 = pipeline.newTimeSlot();
+	slot0.add([&]()
+	{
+		GPU.setup();
+		GPU.computeStep1();
+		CPU.uploadResource1();
+		GPU.computeStep2();
+	});
+	slot0.wait([&]()
+	{
+		TimeSlot & slot1 = pipeline.newTimeSlot();
+		bool needContinue = false;
+		slot1.add([&]()
+		{
+			CPU.uploadResource2();
+		});
+		slot1.add([&]()
+		{
+			needContinue = GPU.getNewJobs();
+			GPU.computeStep1();
+		});
+		slot1.wait([&]()
+		{
+			if (needContinue)
+			{
+				TimeSlot & slot2 = pipeline.newTimeSlot();
+				slot2.add([&]()
+				{
+					GPU.computeStep3();
+					GPU.computeStep2();
+				});
+				slot2.add([&]()
+				{
+					CPU.uploadResource1();
+				});
+				slot2.wait([&]()
+				{
+					slot0.call();
+				});
+			}
+		});
+	});
+}
+
 int main(int argc, char * const argv[])
 {
+	std::cout << "Testing serial..." << std::endl;
+
+	runSerial();
+
+	std::cout << "Testing parallel..." << std::endl;
+
+	runParallel();
+
 	return 0;
 }
--- NxAsync.h
+++ NxAsync.h
@@ -11,4 +11,83 @@
 
 #include <atomic>
 #include <cassert>
+#include <vector>
+#include <functional>
+#include <future>
 
+class TimeSlot
+{
+protected:
+	std::vector<std::function<void()> > m_functors;
+	std::function<void()> m_waitFunctor;
+	std::vector<std::future<void> > m_promises;
+
+public:
+	TimeSlot()
+	{
+	}
+
+	~TimeSlot()
+	{
+	}
+
+	template <typename FUNC>
+	void add(FUNC func)
+	{
+		m_functors.push_back(func);
+	}
+
+	void wait()
+	{
+		m_promises.resize(m_functors.size());
+
+		for (size_t i = 0; i < m_functors.size(); ++i)
+		{
+			m_promises[i] = std::async(std::launch::async, m_functors[i]);
+		}
+
+		for (size_t i = 0; i < m_functors.size(); ++i)
+		{
+			m_promises[i].wait();
+		}
+	}
+
+	template <typename FUNC>
+	void wait(FUNC func)
+	{
+		m_waitFunctor = func;
+		wait();
+		m_waitFunctor();
+	}
+
+	void call()
+	{
+		m_waitFunctor();
+	}
+};
+
+class Pipeline
+{
+protected:
+	std::vector<TimeSlot *> m_timeSlots;
+
+public:
+	Pipeline()
+	{
+	}
+
+	~Pipeline()
+	{
+		for (size_t i = 0; i < m_timeSlots.size(); ++i)
+		{
+			delete m_timeSlots[i];
+		}
+	}
+
+	TimeSlot & newTimeSlot()
+	{
+		TimeSlot *newSlot = new TimeSlot;
+		m_timeSlots.push_back(newSlot);
+		return *newSlot;
+	}
+};
